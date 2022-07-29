b57fcc695d8bfae3835eabb438bf3faab8593063
tbtb1051 <tbtb1051@ainwith.com>
2022-07-29 17:56:58 +0900

    7/29 7강 중간저장

--- src/hijava/practice/Arr.java
+++ src/hijava/practice/Arr.java
@@ -5,19 +5,82 @@ public class Arr {
 	public static void main(String[] args) {
 //		ex1();
 //		ex2();
-		ex3();
-		
+//		ex3();
+//		ex4();
+//		ex5();
+		ex6();
+
+	}
+
+	private static void ex6() {
+		int upperStart = 65;
+		int lowerStart = 97;
+
+		int[] starts = new int[] { upperStart, lowerStart };
+		char[][] arr = new char[2][26];
+
+		for (int start = 0; start < starts.length; start++) {
+//		for(int start : starts) {
+			for (int i = 0; i < 26; i++) {
+				int x = i + starts[start];
+//			System.out.println(x + "=" + (char)x);
+				arr[start][i] = (char) x;
+			}
+
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+	private static void ex5() {
+		int[] arr1 = { 10, 20, 30, 40, 50 };
+		int[] arr2 = { 1, 2, 3, 4, 5 };
+		System.arraycopy(arr1, 0, arr2, 2, 3);
+
+		for (int i : arr2) {
+			System.out.print(i + " ");
+		}
+	}
+
+	private static void ex4() {
+		Man[] people = new Man[9];
+		int len = people.length;
+		for (int i = 0; i < len; i++) {
+			people[i] = new Man("김" + (i + 1) + "수");
+		}
+
+		Man[] people2 = new Man[len];
+		for (int i = 0; i < len; i++) {
+//			people2[i] = people[i]; // shallow copy 얕은카피
+			people2[i] = new Man(people[i].getName()); // deep copy 깊은카피(clone)
+		}
+
+		people[0].setName("김1111111111"); //
+
+		System.out.println("1= " + people[0].getName());
+		System.out.println("2= " + people2[0].getName());
+
+//		for(Man man : people2) { // people에서 하나씩 쓴다 (for문 돌면서)
+//			System.out.println(man);
+//		}
 	}
 
 	private static void ex3() {
 		Man[] people = new Man[9];
 		int len = people.length;
-		for(int i = 0; i < len; i++) {
-			Man m = new Man("김" + (i + 1) + "수");
-			people[i] = m; 
+		for (int i = 0; i < len; i++) {
+//			Man m = new Man("김" + (i + 1) + "수");
+//			people[i] = m; 
+			people[i] = new Man("김" + (i + 1) + "수");
 		}
-		
-		for(Man man : people) {
+
+		for (Man man : people) { // people에서 하나씩 쓴다 (for문 돌면서)
 			System.out.println(man);
 		}
 	}
@@ -25,7 +88,7 @@ public class Arr {
 	private static void ex2() {
 		String[] strs = new String[100];
 		for (int i = 0; i < strs.length; i++) {
-			strs[i] = "str" + ( i + 1);
+			strs[i] = "str" + (i + 1);
 		}
 
 		printArray(strs);
@@ -37,7 +100,7 @@ public class Arr {
 		for (int i = 0; i < nums.length; i++) {
 			nums[i] = i + 1;
 		}
-		
+
 		printArray(nums);
 	}
 
@@ -46,7 +109,7 @@ public class Arr {
 			System.out.println(nums[i]);
 		}
 	}
-	
+
 	private static void printArray(String[] strs) {
 		for (int i = 0; i < strs.length; i++) {
 			System.out.println(strs[i]);
